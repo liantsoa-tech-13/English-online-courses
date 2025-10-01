9cb5569cea8863d473a697bd41aa850aa8aa4b7b
Liantsoa Tech 13 <ratovonanaharyliantsoa@gmail.com>
2025-10-01 15:13:06 +0300

    first commit

---
+++ src/main/java/org/english/englishonlinecourse/EnglishOnlineCourseApplication.java
@@ -0,0 +1,13 @@
+package org.english.englishonlinecourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishOnlineCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishOnlineCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/english/englishonlinecourse/EnglishOnlineCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.english.englishonlinecourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishOnlineCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
