1a12b2678e8c687f04e83fefc95af93bc6f196e1
Liantsoa Tech 13 <ratovonanaharyliantsoa@gmail.com>
2025-10-16 12:30:35 +0300

    fix(user): dropped fullname to make firstname and lastname separately

--- src/main/resources/db.migration/V1_2__Create_user_table.sql
+++ src/main/resources/db.migration/V1_2__Create_user_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE "user" (
    user_id BIGSERIAL PRIMARY KEY,
    public_id VARCHAR(50) UNIQUE NOT NULL DEFAULT ('USR-' || gen_random_uuid()),
-   fullname VARCHAR NOT NULL,
+   firstname VARCHAR(100) NOT NULL,
+   lastname VARCHAR(100) NOT NULL,
    email VARCHAR UNIQUE NOT NULL,
    password_hash VARCHAR NOT NULL,
    role_id INT NOT NULL REFERENCES role(role_id),
