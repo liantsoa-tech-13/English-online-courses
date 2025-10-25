5d016694cc5002f635359615ee6eb6d9ce490a0b
Liantsoa Tech 13 <ratovonanaharyliantsoa@gmail.com>
2025-10-25 19:35:51 +0300

    refactor(repository): used User instead of UserAdminDto

--- src/main/java/org/english/englishonlinecourse/dao/UserAdminRepository.java
+++ src/main/java/org/english/englishonlinecourse/dao/UserAdminRepository.java
@@ -1,10 +1,10 @@
 package org.english.englishonlinecourse.dao;
 
-import org.english.englishonlinecourse.dto.UserAdminDto;
+import org.english.englishonlinecourse.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface UserAdminRepository extends JpaRepository<UserAdminDto, Long> {
+public interface UserAdminRepository extends JpaRepository<User, Long> {
 }
