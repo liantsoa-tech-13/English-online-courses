32331f90ab3eebb1fb06523e8f1758cd2b91de32
Liantsoa Tech 13 <ratovonanaharyliantsoa@gmail.com>
2025-10-25 16:33:20 +0300

    refactor(User): made the repository interface extends jpa

--- src/main/java/org/english/englishonlinecourse/dao/UserAdminRepository.java
+++ src/main/java/org/english/englishonlinecourse/dao/UserAdminRepository.java
@@ -1,9 +1,10 @@
 package org.english.englishonlinecourse.dao;
 
 import org.english.englishonlinecourse.dto.UserAdminDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface UserAdminRepository {
-    List<UserAdminDto> findAllForAdmin();
+public interface UserAdminRepository extends JpaRepository<UserAdminDto, Long> {
 }
